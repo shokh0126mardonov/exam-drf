3cc5afb0bc826bf9c76218424d20d0fb2920cf53
shokh0126mardonov <shokh0126mardonov@example.com>
2026-01-02 21:39:34 +0500

    user model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    class Role(models.TextChoices):
+        Admin = 'ADMIN' , 'Admin'
+        Doctor = 'DOCTOR' , 'Doctor'
+        Patient = 'PATIENT' , 'Patient'
+
+    role = models.CharField(max_length=10, choices=Role.choices, default=Role.Patient)
+ 
+    @property
+    def is_patient(self):
+        return self.role == self.Role.Patient
+    
+    @property
+    def is_admin(self):
+        return self.role == self.Role.Admin
+    
+    @property
+    def is_doctor(self):
+        return self.role == self.Role.Doctor
