4cbd6fe40bd01e771b00890b76d143082c80e1ba
shokh0126mardonov <shokh0126mardonov@example.com>
2026-01-02 22:19:01 +0500

    feat/doctor-model

--- apps/doctors/models.py
+++ apps/doctors/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from ..users.models import CustomUser
+
+class Doctor(models.Model):
+    class Gender(models.TextChoices):
+        Male = 'MALE','Erkak'
+        Female = 'FEMALE','Ayol'
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    specialization = models.CharField()
+    experience_years = models.IntegerField()
+    gender = models.CharField(choices=Gender.choices)
