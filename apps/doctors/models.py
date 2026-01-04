82ea42849a374e21e6c6fbe7b6d3ece907c39cb8
shokh0126mardonov <shokh0126mardonov@example.com>
2026-01-04 22:24:51 +0500

    feat/patient-profile

--- apps/doctors/models.py
+++ apps/doctors/models.py
@@ -1,14 +1,24 @@
 from django.db import models
 from django.conf import settings
 
-from ..users.models import CustomUser
 
 class Doctor(models.Model):
     class Gender(models.TextChoices):
         Male = 'MALE','Erkak'
         Female = 'FEMALE','Ayol'
 
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name='doctor')
     specialization = models.CharField()
     experience_years = models.IntegerField()
     gender = models.CharField(choices=Gender.choices)
+
+
+class Patient(models.Model):
+    class Gender(models.TextChoices):
+        Male = 'MALE','Erkak'
+        Female = 'FEMALE','Ayol'
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name='patient')
+    phone = models.CharField( max_length=13)
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=6, choices=Gender)
